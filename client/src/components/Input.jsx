dbffbce70e9a9933a773e9b97f2418399153e7a5
Muhammed Nihad C <147719739+mnihadc@users.noreply.github.com>
2026-01-15 09:58:17 +0530

    Delete client/src directory

--- client/src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export default function Input({ label, placeholder, type = "text" }) {
-  return (
-    <div className="input-group">
-      <label>{label}</label>
-      <input type={type} placeholder={placeholder} />
-    </div>
-  );
-}
